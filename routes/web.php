fc77ddb06845c8d9555bf464b30d001a102cadba
Muhammad Ilham Faris <53896024+ilhamfaris13@users.noreply.github.com>
2025-03-05 10:42:00 +0700

    login

--- routes/web.php
+++ routes/web.php
@@ -1,26 +1,42 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\BarangController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
 |
 */
 
 Route::get('/', function () {
     return view('welcome');
 });
-//ROUTE BARANG
-Route::get('/barang', [BarangController::class, 'index'])->name('barang.index'); // Menambahkan route untuk index barang 
-Route::get('/barang/create', [BarangController::class, 'create'])->name('barang.create'); // Menambahkan route untuk create barang
-Route::post('/barang/store', [BarangController::class, 'store'])->name('barang.store'); // Menambahkan route untuk store barang
-Route::get('/barang/{id}/edit', [BarangController::class, 'edit'])->name('barang.edit'); // Menambahkan route untuk edit barang 
-Route::put('/barang/{id}', [BarangController::class, 'update'])->name('barang.update');     // Menambahkan route untuk update barang
-Route::delete('/barang/{id}', [BarangController::class, 'destroy'])->name('barang.destroy'); // Menambahkan route untuk delete barang
-Route::get('/barang/{id}', [BarangController::class, 'show'])->name('barang.show'); // Menambahkan route untuk show barang
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+require __DIR__.'/auth.php';
+
+use App\Http\Controllers\BarangController;
+
+Route::get('/barang', [BarangController::class, 'index'])->name('barang.index');
+Route::get('/barang/create', [BarangController::class, 'create'])->name('barang.create');
+Route::post('/barang', [BarangController::class, 'store'])->name('barang.store');
+Route::get('/barang/{id}', [BarangController::class, 'show'])->name('barang.show');
+Route::get('/barang/{id}/edit', [BarangController::class, 'edit'])->name('barang.edit');
+Route::put('/barang/{id}', [BarangController::class, 'update'])->name('barang.update');
+Route::delete('/barang/{id}', [BarangController::class, 'destroy'])->name('barang.destroy');
+Route::get('/barang/{id}/print', [BarangController::class, 'print'])->name('barang.print');
+Route::get('/barang/{id}/pdf', [BarangController::class, 'pdf'])->name('barang.pdf');
